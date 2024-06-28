666e251416bc7aa9f58a81c6be7df84983cdb2c9
Malayai Wilson <codermalayaiwilson@gmail.com>
2024-06-28 02:21:31 -0700

    Starting code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Water LOAD</h1>
+      <div className="card">
+        <button><a href="/user"> Sign Up </a> </button>
+        <button> Log In </button>
+      </div>
+    </>
+  )
+}
+
+export default App
